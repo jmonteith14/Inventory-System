f54915d66195c14ead7af454abf8a7646dbfe1e4
jmonteith14 <jmonteith14@gmail.com>
2026-05-15 21:03:11 -0300

    Updated folders and database files

--- data/schema.sql
+++ data/schema.sql
@@ -28,31 +28,3 @@ Quantity decimal(20,2),
 Created_Date datetime2 DEFAULT GETDATE(),
 FOREIGN KEY (FK_Item_ID) REFERENCES Items(Item_ID)
 );
-
-
-INSERT INTO Categories
-VALUES ('Snack'), ('Condiment')
-
-INSERT INTO Items
-VALUES
-(1, 'Chips', 2, '2027-10-10', 2),
-(2, 'Ketchup' ,0.5 ,'2026-06-24' ,2),
-(2,'Soy Sauce' ,0.25 ,'2028-03-15',1)
-
-INSERT INTO Transactions (FK_Item_ID, Quantity)
-VALUES
-(1, 4),
-(2, 1),
-(3, 0.5),
-(1, -1),
-(2, -0.25)
-
-INSERT INTO Categories
-VALUES ('Produce')
-
-INSERT INTO Items
-VALUES
-(3, 'Apples', 0.5, '2027-10-10', 1)
-
-USE Inventory_System
-SELECT * FROM Items
---
+++ data/test_data.sql
@@ -0,0 +1,23 @@
+
+USE Inventory_System
+
+INSERT INTO Categories
+VALUES
+('Snack'),
+('Condiment'),
+('Produce')
+
+INSERT INTO Items
+VALUES
+(1, 'Chips', 2, '2027-10-10', 2),
+(2, 'Ketchup' ,0.5 ,'2026-06-24' ,2),
+(2,'Soy Sauce' ,0.25 ,'2028-03-15',1),
+(3, 'Apples', 0.5, '2027-10-10', 1)
+
+INSERT INTO Transactions (FK_Item_ID, Quantity)
+VALUES
+(1, 4),
+(2, 1),
+(3, 0.5),
+(1, -1),
+(2, -0.25)
