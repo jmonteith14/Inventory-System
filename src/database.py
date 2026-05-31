7cc03fa02f426bfdb454d99487095fb13a268f0a
jmonteith14 <jmonteith14@gmail.com>
2026-05-31 13:22:17 -0300

    add: python database creation

--- src/database.py
+++ src/database.py
@@ -1,8 +1,9 @@
 import os
 import pyodbc
 
-driver = "ODBC Driver 17 for SQL Server"
+connection = "{ODBC Driver 17 for SQL Server}"
 server = os.getenv("DB_SERVER", "localhost")
 database = "Inventory_System"
 trusted_connection = "yes"
 
+connection_string = 
